8585546e51f40561e8c84e2c6a60da4fe0aea3b8
nguyenvu2511 <59417691+nguyenvu2511@users.noreply.github.com>
2021-07-02 21:21:52 +0700

    Fist Commit

---
+++ Web Mobile/Controllers/MobileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_Mobile.Models;
+
+namespace Shop_Mobile.Controllers
+{
+    public class MobileController : Controller
+    {
+        // GET: Mobile
+        public ActionResult Index()
+        {
+            var dtmoi = Layspmoi(8);
+            
+                return View(dtmoi);
+            
+            
+        }
+        dbShopMobileDataContext data = new dbShopMobileDataContext();
+
+        private List<SanPham> Layspmoi(int count)
+        {
+            var dt = data.LoaiSanPhams.Where(p => p.MaLSP==1).ToList();
+            {
+                return data.SanPhams.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+            }
+            
+        }
+
+        public ActionResult Details(int id)
+        {
+            var sanPhams = from s in data.SanPhams
+                           where s.MaSP == id
+                           select s;
+
+            return View(sanPhams.Single());
+
+
+        }
+
+
+    }
+}
